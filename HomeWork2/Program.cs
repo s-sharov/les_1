ec2a56331b336bf5814a7be2316e2d41fcbf3299
s.sharov <s.sharov@stud.rksi.ru>
2022-12-15 22:06:19 +0300

    Добавил решение домашнего задания 2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+Console.Write("Введите число:");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите еще число:");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите еще одно число:");
+int numberThree = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введенные вами числа - {numberOne} , {numberTwo} , {numberThree}");
+
+Console.Write("Самое большое число из них равно ");
+if (numberOne > numberTwo)
+{
+    Console.WriteLine(numberOne);
+}
+else if (numberTwo > numberThree)
+{
+    Console.WriteLine(numberTwo);
+}
+else
+{
+    Console.WriteLine(numberThree);
+}
