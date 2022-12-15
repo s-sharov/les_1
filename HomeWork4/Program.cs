3b22e1cc2eafdb9f629717243a4b94de56550cf6
s.sharov <s.sharov@stud.rksi.ru>
2022-12-15 22:46:21 +0300

    Добавил домашнее задание 4

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+Console.Write("Введите числа - ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+Console.Write($"Все чётные числа от 1 до {number}: ");
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count += 2;
+}
