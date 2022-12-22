c8ac2b298d390f38396af6356e33cabb1cf23b1a
s.sharov <s.sharov@stud.rksi.ru>
2022-12-22 15:26:39 +0300

    Добавил ДЗ задача №13 и №15

---
+++ Homework13/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 99 || number < -99)
+{
+    Console.WriteLine($"Третья цифра вашего числа: {SerchThirdDigit(number)}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры в вашем числе нет.");
+}
+
+int SerchThirdDigit(int num)
+{
+    if(num < -99)
+    {
+        num = -num;
+    }
+    int count = num;
+    while(count > 999)
+    {
+        num = num / 10;
+        count = num;
+    }
+    return num % 10;
+}
---
+++ Homework15/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет */
+
+
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 0 && number < 8)
+{
+    if(SerchWeekendsDays(number))
+    {
+        Console.WriteLine("Данный день недели является выходным.");
+    }
+    else
+    {
+        Console.WriteLine("Данный день недели не является выходным.");
+    }
+}
+else
+{
+    Console.WriteLine("Нет дня недели с таким номером.");
+}
+
+bool SerchWeekendsDays(int num)
+{
+    switch(num)
+    {
+        case 6:
+            return true;
+        case 7:
+            return true;
+        default:
+            return false;
+    }
+}
