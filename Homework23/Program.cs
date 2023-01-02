92112d34a353b4cef4e29db205c545d78afd5086
s.sharov <s.sharov@stud.rksi.ru>
2023-01-02 22:54:38 +0300

    Add Homework23

---
+++ Homework23/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+5 ->
+1 | 1
+2 | 8
+3 | 27
+4 | 64
+5 | 125 */
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+TableSquare(number);
+
+void TableSquare(int num)
+{
+    int count = 1;
+    while(count <= num)
+    {
+        Console.WriteLine($"{count}\t{Math.Pow(count, 3)}");
+        count++;
+    }
+}
