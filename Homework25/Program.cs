72e027ba377343868e45ba250d2832c84187fa45
Sergey Sharov <s.sharov@stud.rksi.ru>
2023-01-14 15:08:21 +0300

    Add homework25

---
+++ Homework25/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{numberA}, {numberB} -> {ExponentiateNumber(numberA, numberB)}");
+
+int ExponentiateNumber(int num, int degree)
+{
+    int result = num;
+    for(int i = 1; i < degree; i++)
+        result *= num;
+    return result;
+}
