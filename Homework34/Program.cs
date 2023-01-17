e46e2cb3b9c67636886d3e886ea20d8892896589
Sergey Sharov <s.sharov@stud.rksi.ru>
2023-01-17 20:14:19 +0300

    Add Homework34

---
+++ Homework34/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int[] CreateArrayRandomInt(int size)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 999);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1)
+            Console.Write(arr[i] + ", ");
+        else 
+            Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int FindNumEvenElemArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+int[] array = CreateArrayRandomInt(10);
+PrintArray(array);
+
+int count = FindNumEvenElemArray(array);
+
+Console.WriteLine($"Количество четных цисел в массиве: {count}");
