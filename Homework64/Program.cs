c0c8f74025ba62e5b9a7523716283d4781746bbe
Sergey Sharov <s.sharov@stud.rksi.ru>
2023-02-05 16:17:38 +0300

    Add Homework64

---
+++ Homework64/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) 
+    Console.WriteLine("Вы ввели не натуральное число");
+else
+    NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+}
---
+++ Task006'/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+/* Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) 
+    Console.WriteLine("Вы ввели не натуральное число");
+else
+    NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+    
+} */
+
+/* Задача 65: Задайте значения M и N. 
+Напишите программу, которая выведет 
+все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5" 
+M = 4; N = 8 -> "4, 5, 6, 7, 8" */
+
+/* Console.Write("Введите натуральное число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите еще одно натуральное число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if (numberOne < 0 || numberTwo < 0 || numberOne > numberTwo) 
+    Console.WriteLine("Вы ввели не натуральное число или первое число больше второго");
+else
+    NaturalNumbers(numberOne, numberTwo);
+
+void NaturalNumbers(int numOne, int numTwo)
+{
+    if (numOne > numTwo) return;
+    Console.Write($"{numOne} ");
+    NaturalNumbers(numOne + 1, numTwo);
+} */
+
+/* Задача 67: Напишите программу, которая будет 
+принимать на вход число и возвращать сумму его цифр.
+453 -> 12 
+45 -> 9 */
